88ecaec4c50b97a863b83b26dc014da3bb938659
Aleksandr Masko <amasko18@gmail.com>
2022-03-10 22:24:00 +0300

    commit

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -1,11 +1,34 @@
-
+import { useParams } from "react-router-dom";
+import { useEffect } from "react/cjs/react.production.min";
+import { useContext } from "react/cjs/react.production.min";
+import { ShopContext } from "../context/shopContext";
+import { Text, Div, Col, Row, Container } from 'atomize';
 
 
 const ProductPage = () => {
+
+let {id} = useParams();
+const {fetchProductWithId, addItemToCheckout, product} = useContext(ShopContext)
+
+useEffect(() => {
+    fetchProductWithId(id)
+    return () => {
+
+    };
+}, [fetchProductWithId, id])
+    if (!product.title) return <div>Loading</div>
     return(
-        <div>
-            ProductPage
-        </div>
+        <Container>
+            <Row>
+              <Col>
+                 <Div bgImg = {product.images[0].src} bgSize = 'cover' bgPos = 'center center' h = '40rem' />
+              </Col>  
+              <Col>
+                <Text>{product.title}</Text>
+                <Text>{`$`}</Text>
+              </Col>  
+            </Row>
+        </Container>
     )
 }
 
