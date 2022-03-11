42b1b19e0c738cc0d69c9e44ea7f9dd72c18c0af
Aleksandr Masko <amasko18@gmail.com>
2022-03-11 21:16:41 +0300

    commit

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,14 +1,3 @@
-import React, {useContext} from 'react'
-import { Container, Anchor, Icon } from 'atomize'
-import { Link } from 'react-router-dom'
-import { ShopContext } from '../context/shopContext'
-
-
-
-const Test = () => {
-    const { openCart } = useContext(ShopContext)
-
-
 
 
 
