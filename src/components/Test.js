72bbd4b97af93cecb610b6958a0c32ed4477efc3
Aleksandr Masko <amasko18@gmail.com>
2022-03-11 21:16:27 +0300

    commit

--- src/components/Test.js
+++ src/components/Test.js
@@ -8,12 +8,7 @@ import { ShopContext } from '../context/shopContext'
 const Test = () => {
     const { openCart } = useContext(ShopContext)
 
-    return (
-        <>  
-            <Container d="flex" flexDir="row" p="2rem" justify="space-between" >
-                <Link to="/"><Icon name="Store" size="30px" color="black500" /></Link>
-                <Anchor onClick={() => openCart()}><Icon name="Bag" size="20px" color="black500" /></Anchor>
-            </Container>
+
 
 
 
