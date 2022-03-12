b6f368b7fcefe9ab68ba6f494d0bdc174cd33323
Aleksandr Masko <amasko18@gmail.com>
2022-03-12 17:35:34 +0300

    commit

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,46 +1,48 @@
-import React, {useContext, useEffect} from 'react';
-import { ShopContext } from '../context/shopContext';
-import { Container, Text, Row, Col, Div } from 'atomize';
-import { NavLink } from 'react-router-dom';
+import React, { useContext, useEffect }  from 'react'
+import { ShopContext } from '../context/shopContext'
+import { Text, Div, Row, Col, Container } from "atomize";
+import { Link } from 'react-router-dom'
+import Loading from '../components/Loading'
+
 
 const HomePage = () => {
-
-    const { fetchAllProducts, products } = useContext(ShopContext)
+    const {fetchAllProducts, products} = useContext(ShopContext)
 
     useEffect(() => {
         fetchAllProducts()
         return () => {
+            // cleanup
+        };
+    }, [fetchAllProducts])
 
-        }
-    },[fetchAllProducts])
-
-    if(!products) return <div>loading</div>
-
-    return(
-          <Container>
-              <Row>
-                  {products.map(product => (
-                      <Col key = {product.id} size = '3'>
-                          <Text>
-                             <NavLink to = {`/product/${product.id}`}>
-                                 <Div p = '2rem'>
-                                    <Div
-                                    h = '20rem'
-                                    bgImg = {product.images[0].src}
-                                    bgSize = 'cover'
-                                    bgPos = 'center center'
-                                    />
-                                    <Text>{product.title}</Text>    
-                                    <Text>${product.variants[0].price}</Text>    
+    if (!products) return <Loading />
+    return (
+        <Container>
+            <Row>
+                {products.map(product => (
+                    <Col key={product.id} size="3" >
+                        <Link to={`/product/${product.id}`} style={{ textDecoration: 'none' }}>
+                            <Div p="2rem">
+                                <Div 
+                                    h="20rem"
+                                    bgImg={product.images[0].src}
+                                    bgSize="cover"
+                                    bgPos="center center"
+                                    shadow="3"
+                                    hoverShadow="4"
+                                    transition="0.3s"
+                                    m={{ b: "1.5rem" }}
+                                    >
                                 </Div>
-                                 </NavLink>
-
-                          </Text>
-                      </Col>
-                  ))}
-              </Row>
-          </Container>  
+                                <Text tag="h1" textWeight="300" textSize="subheader" textDecor="none" textColor="black500">{product.title}</Text>
+                                <Text tag="h2" textWeight="300" textSize="body" textDecor="none" textColor="gray500">${product.variants[0].price}</Text>
+                            </Div>
+                        </Link>
+                    </Col>
+                ))}
+            </Row>
+        </Container>
     )
 }
 
-export default HomePage;
+export default HomePage
